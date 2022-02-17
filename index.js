a8ccffc5736e7292d1bb25085b899b7e1c1d1813
Blaise Igiraneza <blaiseigiraneza@gmail.com>
2022-02-17 13:48:41 +0200

    day 2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 console.log(countEl)
-
+console.log(saveEl)
 let count = 0
 
 function increment() {
-count ++   // same as count = count + 1 
-countEl.innerText = count 
+count ++   // same as count = count + 1 or count += 1
+countEl.textContent = count 
 console.log(count)
 
 }
@@ -14,4 +15,13 @@ console.log(count)
 function save() {
     console.log(count)
 }
+
+//Save function 
+
+function save() {
+     let countStr = count + " - " 
+     saveEl.textContent += countStr
+
+}
+
 
